1c258e307ad557659943ee06228590339d7db892
Guilherme Araujo <guilherme.araujo.pereira@everis.com>
2021-03-30 17:49:40 -0300

    Criação da chamada de API para request dos dados

---
+++ app/src/main/java/everis/com/becamobilemovie/Api/MovieApi.kt
@@ -0,0 +1,13 @@
+package everis.com.becamobilemovie.Api
+
+import everis.com.becamobilemovie.DataClass.Movies
+import retrofit2.http.GET
+
+interface MovieApi {
+    companion object{
+        const val API_KEY = "a9db10997b829bfe25d6c577d6817f3b"
+        const val ROTA = "3/trending/movie/week?api_key="
+    }
+    @GET(ROTA + API_KEY)
+    fun getAllMovies(): List<Movies>
+}
---
+++ app/src/main/java/everis/com/becamobilemovie/Api/MovieRestApiTask.kt
@@ -0,0 +1,16 @@
+package everis.com.becamobilemovie.Api
+
+import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MovieRestApiTask {
+    companion object{
+        const val BASE_URL = "https://api.themoviedb.org/"
+    }
+    private fun movieProvider(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    fun retrofiApi(): MovieApi = movieProvider().create(MovieApi::class.java)
+}
