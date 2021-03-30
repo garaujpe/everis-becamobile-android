fc935e3249bbd00c9f285daa65da02cb7576e6d4
Guilherme Araujo <guilherme.araujo.pereira@everis.com>
2021-03-30 15:25:59 -0300

    Add gson framework e criação da dataclass MoviesResult e ajuste na dataclass Movies

--- app/src/main/java/everis/com/becamobilemovie/Movies.kt
+++ app/src/main/java/everis/com/becamobilemovie/Movies.kt
@@ -1,14 +1,15 @@
 package everis.com.becamobilemovie
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Movies(
-    var id: Int,
-    var name: String,
-    var capa: String,
-    var sinopse: String,
-    var raiting: Double,
-    var lançamento: String
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val name: String,
+    @SerializedName("poster_path") val capa: String?,
+    @SerializedName("overview") val sinopse: String,
+    @SerializedName("vote_average") val raiting: Double,
+    @SerializedName("release_date") val lançamento: String
 ): Parcelable
---
+++ app/src/main/java/everis/com/becamobilemovie/MoviesResult.kt
@@ -0,0 +1,11 @@
+package everis.com.becamobilemovie
+
+import com.google.gson.annotations.SerializedName
+
+data class MoviesResult(
+    @SerializedName("pages") val pages: Int,
+    @SerializedName("results") val results: List<Movies>,
+    @SerializedName("total_pages") val totalPages: Int,
+    @SerializedName("total_results") val totalResults: Int
+
+)
